eb9e922e91b6538aaacc6305724a769f0ddef0ed
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-21 19:03:38 +0200

    Working on Events Pages with dynamiclly paths and data

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,7 +1,8 @@
-import Link from 'next/link'
-
 import classes from '../../styles/event-item.module.css';
 import Button from '../ui/button';
+import DateIcon from '../icons/date-icon'
+import AdressIcon from '../icons/address-icon'
+import ArrowRightIcon from '../icons/arrow-right-icon'
 
 const EventItem = ({ item }) => {
     const { title, image, date, location, id } = item
@@ -23,14 +24,19 @@ const EventItem = ({ item }) => {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AdressIcon />
                         <address>{formattedAdress}</address>
                     </div>
                 </div>
                 <div className={classes.actions}>
-                    <Button link={exploreLink} children="Explore Event" />
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+                        <span className={classes.icon}><ArrowRightIcon /></span>
+                    </Button>
                 </div>
             </div>
         </li>
--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,8 +1,29 @@
+import { useRouter } from 'next/router'
+import { Fragment } from 'react'
+
+import { getEventById } from '../../dummy-data'
+import EventSummary from '../../components/EventDetail/EventSummary'
+import EventLogistics from '../../components/EventDetail/EventLogistics'
+import EventContent from '../../components/EventDetail/EventContent'
+
 const EventDetailPage = () => {
+    const router = useRouter()
+
+    const eventId = router.query.eventId
+    const event = getEventById(eventId)
+
+    if(!event) {
+        return <p>No event found</p>
+    }
+
     return (
-        <div>
-            Event Detail Page
-        </div>
+        <Fragment>
+           <EventSummary title={event.tile} />
+           <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+           <EventContent>
+               <p>{event.description}</p>
+           </EventContent>
+        </Fragment>
     )
 }
 
